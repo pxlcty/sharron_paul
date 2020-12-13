1a16d1a197072e3bc72b6bf0b029a9c4a86324e2
Michael McCracken <pxlcty@gmail.com>
2020-12-13 03:39:39 -0500

    Create homepage initial layout.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'pages#home'
+  get 'about', to: 'pages#about'
+
+  # to build : 
+  #resources :events
+  #resources :media
+  #resources :admin, except: [:destroy] I think... (only one user admin)
+  #get 'admin-login', to 'sessions#new'
+  #post 'admin-login', to 'sessions#create'
+  #delete 'admin-logout', to 'sessions#destroy'
+
 end
