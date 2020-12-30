2f67b93949434130c25598d91ea65cc592f97d39
Michael McCracken <pxlcty@gmail.com>
2020-12-30 00:35:51 -0500

    Add homepage query for 3 events ascending upon :time variable. Bug with :time saving/creation

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,12 @@ class PagesController < ApplicationController
 
     def home
         @bio = Detail.first
-        @events = Event.last(3)
+        #@events = Event.last(3)
+        #@events = Event.order(time: :asc).first(3)
+        @events = Event.where('time > ?', Date.today).order(time: :asc).first(3)
+        #@events = Event.where(time: Time.now.midnight - 15.day).order(time: :asc).first(3)
+        #now_time = DateTime.now
+        #@events = Event.where(:time > :now_time).first(3)
     end
 
     def about
@@ -10,3 +15,12 @@ class PagesController < ApplicationController
     end
 
 end
+
+# Comment.where(:created_at => @selected_date.beginning_of_day..@selected_date.end_of_day)
+# @due_this_week = current_user.tasks.where(due_date: 1.week.ago..Date.today)
+
+#some_date = Time.now
+#Item.where("created_at >= :date OR updated_at >= :date", date: some_date.tomorrow.beginning_of_day)
+
+#Model.where(needed_column: [Time.now.at_beginning_of_day, Time.now.end_of_day])
+#Client.where(created_at: (Time.now.midnight - 1.day)..Time.now.midnight)
